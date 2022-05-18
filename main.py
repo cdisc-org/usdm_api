d95bec7d2b2b311b29dd9de2d20decc386e8dada
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-05-18 10:33:48 +0200

    Refactoring

--- main.py
+++ main.py
@@ -6,7 +6,23 @@ from store.store import Store
 
 store = Store()
 
-class Amendment(BaseModel):
+class ApiBaseModel(BaseModel):
+
+  def check_and_save(self, item):
+    if item == None:
+      return None
+    elif isinstance(item, str):
+      return item
+    else:
+      return item.save()
+
+  def save(self):
+    self.uuid = str(uuid4())
+    print("Class:", self.__class__.__name__)
+    store.put(self.__class__.__name__, vars(self), self.uuid)
+    return self.uuid
+
+class Amendment(ApiBaseModel):
   uuid: Union[str, None] = None
   amendment_effective_date: str
   amendment_version: str
@@ -18,32 +34,48 @@ class StudyProtocol(BaseModel):
   public_title: str
   scientific_title: str
   version: str
-  study_protocol_amendments: Union[List[Amendment], None] = []
+  study_protocol_amendments: Union[List[Amendment], List[str], None] = []
 
-class Code(BaseModel):
+  def save(self):
+    self.uuid = str(uuid4())
+    for idx, amendment in enumerate(self.study_protocol_amendments):
+      self.study_protocol_amendments[idx] = self.check_and_save(amendment)
+    store.put(self.__class__.__name__, vars(self), self.uuid)
+    return self.uuid
+
+class Code(ApiBaseModel):
   uuid: Union[str, None] = None
   code: str
   code_system: str
   code_system_version: str
   decode: str
 
-class StudyIdentifier(BaseModel):
+class StudyIdentifier(ApiBaseModel):
   uuid: Union[str, None] = None
   study_identifier_desc: str
   study_identifier_name: str
   org_code: str
 
-class Study(BaseModel):
+class Study(ApiBaseModel):
   uuid: Union[str, None] = None
   study_title: str
   study_version: str
   study_status: str
   study_protocol_version: str
-  study_type: Code
-  study_phase: Code
-  study_identifier: Union[List[StudyIdentifier], None] = []
-  study_protocol_reference: Union[str, None] = None
-  #study_status: Union[str, None] = None
+  study_type: Union[Code, str, None]
+  study_phase: Union[Code, str, None]
+  study_identifier: Union[List[StudyIdentifier], List[str], None] = []
+  study_protocol_reference: Union[StudyProtocol, str, None] = None
+
+  def save(self):
+    self.uuid = str(uuid4())
+    self.study_type = self.check_and_save(self.study_type)
+    self.study_phase = self.check_and_save(self.study_phase)
+    self.study_protocol_reference = self.check_and_save(self.study_protocol_reference)
+    for idx, identifier in enumerate(self.study_identifier):
+      self.study_identifier[idx] = self.check_and_save(identifier)
+    store.put(self.__class__.__name__, vars(self), self.uuid)
+    return self.uuid
 
 app = FastAPI()
 
@@ -51,28 +83,20 @@ app = FastAPI()
 def read_root():
   return {"Version": "0.1", "System": "DDF API Simulator"}
 
-@app.get("/study/{uuid}")
-def read_item(uuid: str):
-  return store.get("Study", uuid)
-
 @app.get("/study/")
-def read_item():
+def list_items():
   return store.list("Study")
 
 @app.post("/study/")
 async def create_item(study: Study):
-  study.uuid = str(uuid4())
-  study.study_type.uuid = str(uuid4())
-  study.study_phase.uuid = str(uuid4())
-  for identifier in study.study_identifier:
-    identifier.uuid = str(uuid4())
-  store.put("Study", vars(study), study.uuid)
+  study.save()
   return study.uuid
+
+@app.get("/study/{uuid}")
+def read_item(uuid: str):
+  return store.get("Study", uuid)
 
 @app.post("/protocol/")
 async def create_item(protocol: StudyProtocol):
-  protocol.uuid = str(uuid4())
-  for amendment in protocol.study_protocol_amendments:
-    amendment.uuid = str(uuid4())
-  store.put("Protocol", vars(protocol), protocol.uuid)
+  protocol.save()
   return protocol.uuid
