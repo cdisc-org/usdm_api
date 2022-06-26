fc571c4f03a5a3240890705300d467797383ff5a
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-06-26 12:12:20 +0200

    Fix simple study

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ from study.simple_study import *
 from study.ddr import *
 
 studies = [
-  #SimpleStudy, 
-  DDR
+  SimpleStudy, 
+  #DDR
   ]
 items = [
   "studies", "study_identifiers", "organisations", "study_protocol_versions", "study_arms", "study_epochs", 
@@ -16,14 +16,13 @@ if __name__ == "__main__":
   service = Service(sys.argv)
   for study in studies:
     data = study.json()
-    print(data)
     uuid = service.post("study_definitions", data)
     service.get("study_definitions", uuid)
     service.get("studies", uuid)
-  for item in items:
-    uuids = service.get("%s/list" % (item))
-    service.get(item, uuids[0])
-  uuid = service.get("study_definitions?identifier=NCT04298023")
-  uuid = service.get("study_definitions/list")
-  soa = service.get("study_definitions/soa/%s" % (uuid[0]))
+  #for item in items:
+  #  uuids = service.get("%s/list" % (item))
+  #  service.get(item, uuids[0])
+  #uuid = service.get("study_definitions?identifier=NCT04298023")
+  #uuid = service.get("study_definitions/list")
+  #soa = service.get("study_definitions/soa/%s" % (uuid[0]))
   
