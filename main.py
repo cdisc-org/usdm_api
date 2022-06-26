a9cb7ffe246669b475b14271ec1a39bd510db435
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-06-26 15:58:27 +0200

    Update route

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from model.activity import *
 from model.transition_rule import *
 from model.encounter import *
 
-VERSION = "0.15"
+VERSION = "0.16"
 SYSTEM_NAME = "DDF API Simulator"
 
 tags_metadata = [
