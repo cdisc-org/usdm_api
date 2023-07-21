82b0ad03974b8e781ca319d60bfb9e7bdee54791
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-07-21 10:20:13 +0200

    Redundant id definition

--- model/alias_code.py
+++ model/alias_code.py
@@ -1,9 +1,7 @@
-from pydantic import constr
 from typing import List
 from .api_base_model import ApiBaseModelWithId
 from .code import Code
 
 class AliasCode(ApiBaseModelWithId):
-  id: str = constr(min_length=1)
   standardCode: Code
   standardCodeAliases: List[Code] = []
