dd7fd0480407f4bb3d1b6b65a5191c372a675d08
Jared Schreibman <jschreibman@cdisc.org>
2023-08-16 10:58:18 -0500

    Updated version

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from model.study import *
 from model.wrapper import Wrapper
 from uuid import UUID, uuid4
 
-VERSION = "2.1 Provisional (0.36)"
+VERSION = "2.2 Provisional (0.37)"
 SYSTEM_NAME = "Simple API for DDF"
 
 tags_metadata = [
