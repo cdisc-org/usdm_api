9934a0e2578dfcaf6948f22a3bf37977cab25ef8
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-10-11 07:55:04 +0200

    Fix missing reference

--- model/study_amendment.py
+++ model/study_amendment.py
@@ -1,5 +1,6 @@
 from .api_base_model import ApiBaseModelWithId
 from .study_amendment_reason import StudyAmendmentReason
+from .enrollment import Enrollment
 
 class StudyAmendment(ApiBaseModelWithId):
   number: str
