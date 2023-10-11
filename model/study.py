596c974700cbb9f276fddae068c91726f3d207b5
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-10-11 08:21:51 +0200

    Fix bug, missing relationship

--- model/study.py
+++ model/study.py
@@ -1,7 +1,9 @@
-from typing import Union
+from typing import List, Union
 from .api_base_model import ApiBaseModelWithIdNameLabelAndDesc
 from .study_protocol_document import StudyProtocolDocument
+from .study_version import StudyVersion
 
 class Study(ApiBaseModelWithIdNameLabelAndDesc):
   documentedBy: Union[StudyProtocolDocument, None] = None
+  versions: List[StudyVersion]
   
