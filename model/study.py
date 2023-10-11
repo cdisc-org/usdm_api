655e85413fab31186a4bba30eaa916a32d55ffe1
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-10-11 07:51:19 +0200

    Bug fix

--- model/study.py
+++ model/study.py
@@ -1,6 +1,6 @@
 from typing import List, Union
 from .api_base_model import ApiBaseModelWithIdNameLabelAndDesc
-from study_protocol_document import StudyProtocolDocument
+from .study_protocol_document import StudyProtocolDocument
 
 class Study(ApiBaseModelWithIdNameLabelAndDesc):
   documentedBy: Union[StudyProtocolDocument, None] = None
