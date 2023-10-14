429d289ebb6510626d113d5be03710b2d861ec02
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-10-14 10:54:57 +0200

    Make name and description syntax consostent

--- model/study.py
+++ model/study.py
@@ -1,5 +1,5 @@
 from typing import List, Union
-from pydantic import Field, constr
+from pydantic import Field
 from .api_base_model import ApiBaseModel
 from .study_protocol_document import StudyProtocolDocument
 from .study_version import StudyVersion
@@ -7,8 +7,8 @@ from uuid import UUID
 
 class Study(ApiBaseModel):
   id: Union[UUID, None] = None
-  name: str #= constr(min_length=1)
-  description: Union[str, None] = None
-  label: Union[str, None] = None
+  name: str = Field(min_length=1)
+  description: str = ""
+  label: str = ""
   versions: List[StudyVersion] = []
   documentedBy: Union[StudyProtocolDocument, None] = None
