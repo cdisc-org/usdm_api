6b1d25381dd3a39c777882febb7d967698e49694
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-12-18 07:35:50 +0100

    Restore id to StudyIdentifier. Also set default for instanceType

--- model/study_identifier.py
+++ model/study_identifier.py
@@ -1,8 +1,8 @@
-from .api_base_model import ApiBaseModel
+from .api_base_model import ApiBaseModelWithId
 from .organization import Organization
 from typing import Literal
 
-class StudyIdentifier(ApiBaseModel):
+class StudyIdentifier(ApiBaseModelWithId):
   studyIdentifier: str
   studyIdentifierScope: Organization
-  instanceType: Literal['StudyIdentifier']
+  instanceType: Literal['StudyIdentifier'] = 'StudyIdentifier'
