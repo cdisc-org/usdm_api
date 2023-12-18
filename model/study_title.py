5dc4670f838cee446836609cb6f60559e9210e1c
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-12-18 08:14:21 +0100

    Fix type, no need for cross reference

--- model/study_title.py
+++ model/study_title.py
@@ -1,7 +1,8 @@
 from typing import Literal
 from .api_base_model import ApiBaseModelWithId
+from .code import Code
 
 class StudyTitle(ApiBaseModelWithId):
   text: str
-  typeId: str
+  type: Code
   instanceType: Literal['StudyTitle'] = 'StudyTitle'
