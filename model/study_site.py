a3f84a1657becf14b007dcb0517aef6c6fb29e69
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-12-18 08:15:02 +0100

    Fix StudySite, no need fro cross reference

--- model/study_site.py
+++ model/study_site.py
@@ -1,6 +1,7 @@
 from typing import Literal, Union
 from .api_base_model import ApiBaseModelWithIdNameLabelAndDesc
+from .geographic_scope import SubjectEnrollment
 
 class StudySite(ApiBaseModelWithIdNameLabelAndDesc):
-  currentEnrollmentId: Union[str, None] = None
+  currentEnrollment: Union[SubjectEnrollment, None] = None
   instanceType: Literal['StudySite'] = 'StudySite'
