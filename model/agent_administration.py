5013fc2ee6d399a33bbf1ead284462a27077b4f3
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-03-13 08:35:17 +0100

    IDMP AliasCode changes

--- model/agent_administration.py
+++ model/agent_administration.py
@@ -2,12 +2,12 @@ from typing import Union
 from .api_base_model import ApiBaseModelWithIdNameLabelAndDesc
 from .quantity import Quantity
 from .administration_duration import AdministrationDuration
-from .code import Code
+from .alias_code import AliasCode
 from typing import Literal
 
 class AgentAdministration(ApiBaseModelWithIdNameLabelAndDesc):
   duration:	AdministrationDuration
   dose:	Union[Quantity, None] = None
-  route:	Code
-  frequency:	Code
+  route:	AliasCode
+  frequency:	AliasCode
   instanceType: Literal['AgentAdministration']
