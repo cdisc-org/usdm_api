ed871a33d6c68d061913b95104ba280be4f8b730
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-03-29 08:22:35 +0100

    Tidy up

--- model/agent_administration.py
+++ model/agent_administration.py
@@ -1,9 +1,8 @@
-from typing import Union
+from typing import Literal
 from .api_base_model import ApiBaseModelWithIdNameLabelAndDesc
 from .quantity import Quantity
 from .administration_duration import AdministrationDuration
 from .alias_code import AliasCode
-from typing import Literal
 
 class AgentAdministration(ApiBaseModelWithIdNameLabelAndDesc):
   duration:	AdministrationDuration
