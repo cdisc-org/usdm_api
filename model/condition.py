aa7595d0d936df11c76d3bf7bcfc4c937001cad5
Dave Iberson-Hurst <daveih@dih-m1-mini.home>
2024-06-11 12:46:48 +0200

    Further tweak for condition

--- model/condition.py
+++ model/condition.py
@@ -2,8 +2,6 @@ from typing import List, Literal, Union
 from .syntax_template import SyntaxTemplate
 
 class Condition(SyntaxTemplate):
-  text: str
-  dictionaryId: Union[str, None] = None
   contextIds: List[str] = []
   appliesToIds: List[str] = []
   instanceType: Literal['Condition']
