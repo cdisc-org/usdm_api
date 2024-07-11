c8f259ac8d55190f82d02d33551e706a71a99fa0
Dave Iberson-Hurst <daveih@dih-m1-mini.home>
2024-07-11 15:34:37 +0200

    Add in renames and new notes attribute

--- model/study_definition_document_version.py
+++ model/study_definition_document_version.py
@@ -3,11 +3,13 @@ from .api_base_model import ApiBaseModelWithId
 from .code import Code
 from .governance_date import GovernanceDate
 from .narrative_content import NarrativeContent
+from .comment_annotation import CommentAnnotation
 
 class StudyDefinitionDocumentVersion(ApiBaseModelWithId):
-  protocolVersion: str
-  protocolStatus: Code
+  version: str
+  status: Code
   dateValues: List[GovernanceDate] = []
   contents: List[NarrativeContent] = []
   childIds: List[str] = []
+  notes: List[CommentAnnotation] = []
   instanceType: Literal['StudyDefinitionDocumentVersion']
