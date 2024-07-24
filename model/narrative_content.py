dc8d5def322ccdc0b12d5833190d8cbeb21856bb
Dave Iberson-Hurst <daveih@dih-m1-mini.home>
2024-07-24 07:07:20 +0200

    Remove white space

--- model/narrative_content.py
+++ model/narrative_content.py
@@ -4,7 +4,7 @@ from .api_base_model import ApiBaseModelWithIdAndName
 class NarrativeContentItem(ApiBaseModelWithIdAndName):
   text: str
   instanceType: Literal['NarrativeContentItem']
-  
+
 class NarrativeContent(ApiBaseModelWithIdAndName):
   sectionNumber: Union[str, None] = None
   sectionTitle: Union[str, None] = None
