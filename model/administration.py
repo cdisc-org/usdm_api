303affe0b719e686c8e8e3f8e946dd7bdd20aac5
Dave Iberson-Hurst <115090009+dih-cdisc@users.noreply.github.com>
2024-08-29 16:17:33 +0200

    Add link into new classes

--- model/administration.py
+++ model/administration.py
@@ -1,7 +1,8 @@
-from typing import List, Literal
+from typing import List, Union, Literal
 from .api_base_model import ApiBaseModelWithIdNameLabelAndDesc
 from .quantity import Quantity
 from .administration_duration import AdministrationDuration
+from .administrabel_product import AdministrableProduct
 from .alias_code import AliasCode
 from .comment_annotation import CommentAnnotation
 
@@ -9,6 +10,7 @@ class Administration(ApiBaseModelWithIdNameLabelAndDesc):
   duration:	AdministrationDuration
   dose:	Quantity
   route: AliasCode
-  frequency:AliasCode
+  frequency: AliasCode
+  administrableProduct: Union[AdministrableProduct, None] = None
   notes: List[CommentAnnotation] = []
   instanceType: Literal['AgentAdministration']
