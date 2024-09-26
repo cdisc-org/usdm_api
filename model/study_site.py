f9e8b34d2dadcdd134d07669aa31b427598b3c45
Richard Marshall <113628824+ASL-rmarshall@users.noreply.github.com>
2024-09-26 12:21:13 +0100

    Protocol Amendment changes, tweak cardinality in Masking

--- model/study_site.py
+++ model/study_site.py
@@ -1,5 +1,7 @@
-from typing import Literal
+from typing import Literal, Union
 from .api_base_model import ApiBaseModelWithIdNameLabelAndDesc
+from .code import Code
 
 class StudySite(ApiBaseModelWithIdNameLabelAndDesc):
+  country: Code
   instanceType: Literal['StudySite']
