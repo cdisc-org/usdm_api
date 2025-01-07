949add05a4d6d5c3b5b8e56e5649c72eff89f01a
Richard Marshall <113628824+ASL-rmarshall@users.noreply.github.com>
2025-01-07 14:19:21 +0000

    Make elements required in StudyCell (DDF-RA #559)

--- model/study_cell.py
+++ model/study_cell.py
@@ -4,5 +4,5 @@ from .api_base_model import ApiBaseModelWithId
 class StudyCell(ApiBaseModelWithId):
   armId: str
   epochId: str
-  elementIds: List[str] = []
+  elementIds: List[str]
   instanceType: Literal['StudyCell']
