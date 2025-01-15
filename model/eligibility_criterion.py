6f5812ad8c44307fb837bea72499cc8a7986bec9
Richard Marshall <113628824+ASL-rmarshall@users.noreply.github.com>
2025-01-15 13:18:07 +0000

    Add name, label, description and notes to EligibilityCriterion (DDF-RA #430)

--- model/eligibility_criterion.py
+++ model/eligibility_criterion.py
@@ -1,14 +1,16 @@
-from typing import Literal, Union
-from .api_base_model import ApiBaseModelWithId
+from typing import Literal, Union, List
+from .api_base_model import ApiBaseModelWithIdNameLabelAndDesc
 from .syntax_template import SyntaxTemplate
+from .comment_annotation import CommentAnnotation
 from .code import Code
 
-class EligibilityCriterion(ApiBaseModelWithId):
+class EligibilityCriterion(ApiBaseModelWithIdNameLabelAndDesc):
   category: Code
   identifier: str
   criterionItemId: str
   nextId: Union[str, None] = None
   previousId: Union[str, None] = None
+  notes: List[CommentAnnotation] = []
   instanceType: Literal['EligibilityCriterion']
 
 class EligibilityCriterionItem(SyntaxTemplate):
