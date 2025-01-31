b230999db1a7159656d62db420fd7efe47f827ec
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-01-31 05:58:39 +0100

    Forgot to add url to Class Extension

--- model/extension_class.py
+++ model/extension_class.py
@@ -1,7 +1,9 @@
 from typing import Literal, List
+from pydantic import Field
 from .api_base_model import ApiBaseModelWithId
 from .extension_attribute import ExtensionAttribute
 
 class ExtensionClass(ApiBaseModelWithId):
+  url: str = Field(min_length=1)
   attributes: List[ExtensionAttribute]
   instanceType: Literal['ExtensionClass']
