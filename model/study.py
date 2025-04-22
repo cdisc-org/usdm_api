74ffffdfaca76e14698b418594d953b8dd4946cb
Richard Marshall <113628824+ASL-rmarshall@users.noreply.github.com>
2025-04-22 15:16:36 +0100

    Add extensionAttributes to Study (DDF-RA #629)

--- model/study.py
+++ model/study.py
@@ -1,12 +1,14 @@
 from typing import List, Literal, Union
 from pydantic import Field
 from .api_base_model import ApiBaseModel
+from .extension import ExtensionAttribute
 from .study_definition_document import StudyDefinitionDocument
 from .study_version import StudyVersion
 from uuid import UUID
 
 class Study(ApiBaseModel):
   id: Union[UUID, None] = None
+  extensionAttributes: List[ExtensionAttribute] = []
   name: str = Field(min_length=1)
   description: Union[str, None] = None
   label: Union[str, None] = None
