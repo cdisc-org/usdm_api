64ee234d4af6bc8181527408201563eba5df3200
Richard Marshall <113628824+ASL-rmarshall@users.noreply.github.com>
2025-04-24 17:18:05 +0100

    New Duration class (DDF-RA #635) and add notes to StudyRole (DDF-RA #634)

--- model/administration_duration.py
+++
@@ -1,9 +0,0 @@
-from typing import Literal, Union
-from .api_base_model import ApiBaseModelWithIdAndDesc
-from .quantity_range import Quantity
-
-class AdministrationDuration(ApiBaseModelWithIdAndDesc):
-  quantity:	Union[Quantity, None] = None
-  durationWillVary: bool
-  reasonDurationWillVary:	Union[str, None] = None
-  instanceType: Literal['AdministrationDuration']
